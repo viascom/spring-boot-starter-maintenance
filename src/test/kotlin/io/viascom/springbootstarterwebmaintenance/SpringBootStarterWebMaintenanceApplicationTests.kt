5aff79e789827aecd2cb7e5250f39afbe05ec1c2
Nikola Stankovic <nikola.stankovic@umb.ch>
2021-12-15 14:33:00 +0100

    init: bootstrapping

---
+++ src/test/kotlin/io/viascom/springbootstarterwebmaintenance/SpringBootStarterWebMaintenanceApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.viascom.springbootstarterwebmaintenance
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootStarterWebMaintenanceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
